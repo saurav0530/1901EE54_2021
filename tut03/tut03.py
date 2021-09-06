51787de2ec1c8c3f40e5bb924ffa1bdc072a823f
sauravkumar0530 <72290145+sauravkumar0530@users.noreply.github.com>
2021-09-06 15:43:49 +0530

    Tut 03 final commit
    For sake of uploading, I took the size of regtable_old.csv to 250 and accordingly is the size of both output folder.

---
+++ tut03/tut03.py
@@ -0,0 +1,52 @@
+import os
+os.system("cls")
+
+# Function which sets headers and data of each file of each folder
+def addDataToCorrespFile(type,name,data,methods):
+    check=0
+    if(type==1):
+        filename="./output_individual_roll/"+name+".csv"
+    else:
+        filename="./output_by_subject/"+name+".csv"
+    file = open(filename, methods)
+    for i in data:
+        if(check):
+            file.write(',')
+        file.write(i)
+        check+=1
+    file.close()
+
+# Function which extracts the number of unique students and subjects and creates blank file for each
+# and sets headers of each file
+def setSchema_for_each_file_of_both_folder():
+    f=open('regtable_old.csv', 'r')
+    uniqueRoll = set()
+    uniqueSubject = set()
+    results = ["rollno","register_sem","subno","sub_type\n"]
+    for line in f:
+        if(line.split(',')[0] != "rollno"):
+            uniqueRoll.add(line.split(',')[0])
+            uniqueSubject.add(line.split(',')[3])
+            
+    for i in uniqueRoll:
+        addDataToCorrespFile(1,i,results,"w")       # Sets headers of each file of output_individual_roll
+    for i in uniqueSubject:
+        addDataToCorrespFile(2,i,results,"w")       # Sets headers of each file of output_by_subject
+    f.close()
+
+# Function which extracts data from given csv file and add it to corresponding file of corresponding folder
+def add_data_to_each_file_of_both_folder():
+    f=open('regtable_old.csv', 'r')
+    for data in f:
+        studentData = data.split(',');
+        if(studentData[0]=="rollno"):
+            continue
+        results = [studentData[0],studentData[1],studentData[3],studentData[8]]
+        addDataToCorrespFile(1,results[0],results,"a")      # adds data of each file of output_individual_roll
+        addDataToCorrespFile(2,results[2],results,"a")      # adds data of each file of output_by_subject
+    f.close()
+
+
+# Function call made
+setSchema_for_each_file_of_both_folder()
+add_data_to_each_file_of_both_folder()
